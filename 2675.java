7c12c1d0000ba3080579ffa739d9a8b4ed5d77b4
devcs96 <katd6@naver.com>
2022-03-14 23:02:40 +0900

    solve($2657) : 기초 구현 문제

---
+++ 2675.java
@@ -0,0 +1,28 @@
+// https://www.acmicpc.net/problem/2675
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer inputNum = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < inputNum ; i++){
+            String[] param = br.readLine().split(" ");
+            Integer replicationNum = Integer.parseInt(param[0]);
+            String target = param[1];
+            for(int j = 0; j < target.length() ; j++){
+                for(int k = 0; k < replicationNum ; k++) {
+                    sb.append(target.charAt(j));
+                }
+            }
+            System.out.println(sb.toString());
+            sb.setLength(0);
+        }
+    }
+}
